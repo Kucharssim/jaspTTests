28577c3dc9a0063c396133ac4454fa0161142af1
Kucharssim <kucharssim@gmail.com>
2022-04-08 11:15:07 +0200

    add mock wrapper for independent samples t-test

---
+++ R/wrapper-ttestindependentsamples.R
@@ -0,0 +1,23 @@
+#' Independent Samples T-test
+#'
+#' @description Conduct a classical independent samples t-test.
+#'
+#' @param data Data frame containing the data.
+#' @param dependent Character vector. Specify the columns that contain the dependent variable.
+#' @param groupBy Character. Specify the grouping variable.
+#'
+#' @examples
+#' independentSamplesTTest(data = sleep, dependent = "extra", groupBy = "group")
+#' @export
+independentSamplesTTest <- function(data, dependent, groupBy) {
+  ttest <- data.frame(dependent = dependent, t = NA, df = NA, p = NA)
+  for(dep in dependent) {
+    formula <- as.formula(paste(dep, groupBy, sep = "~"))
+    res <- t.test(formula, data)
+    ttest[ttest$dependent == dep, "t"]  <- res[["statistic"]]
+    ttest[ttest$dependent == dep, "df"] <- res[["parameter"]]
+    ttest[ttest$dependent == dep, "p"]  <- res[["p.value"]]
+  }
+
+  return(list(ttest = ttest))
+}
